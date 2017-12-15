945a4402a538fb297970203d4a0395374acb1690
mnichangxin <mnichangxin@163.com>
2017-12-15 22:15:40 +0800

    commit

---
+++ ActivityLifeSCycleTest/app/src/main/java/com/example/activitylifescycletest/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.activitylifescycletest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+
+        Button button1 = (Button) findViewById(R.id.start_normal_activity);
+        Button button2 = (Button) findViewById(R.id.start_dialog_activity);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
