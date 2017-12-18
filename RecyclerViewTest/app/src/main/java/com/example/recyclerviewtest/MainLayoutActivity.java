b506c4d6d33fbd74bc4bc0ed4c1082a5d4513bfc
mnichangxin <mnichangxin@163.com>
2017-12-18 22:14:52 +0800

    commit

--- RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainLayoutActivity.java
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainLayoutActivity.java
@@ -2,12 +2,114 @@ package com.example.recyclerviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainLayoutActivity extends AppCompatActivity {
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private void initFruits() {
+        for (int i = 0; i < 2; i++) {
+            Fruit apple = new Fruit("Apple", R.mipmap.ic_launcher);
+            fruitList.add(apple);
+            Fruit banana = new Fruit("Banana", R.mipmap.ic_launcher);
+            fruitList.add(banana);
+            Fruit orange = new Fruit("Orange", R.mipmap.ic_launcher);
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit("Watermelon", R.mipmap.ic_launcher);
+            fruitList.add(watermelon);
+            Fruit pear = new Fruit("Pear", R.mipmap.ic_launcher);
+            fruitList.add(pear);
+            Fruit grape = new Fruit("Grape", R.mipmap.ic_launcher);
+            fruitList.add(grape);
+            Fruit pineapple = new Fruit("Pineapple", R.mipmap.ic_launcher);
+            fruitList.add(pineapple);
+            Fruit strawberry = new Fruit("Strawberry", R.mipmap.ic_launcher);
+            fruitList.add(strawberry);
+            Fruit cherry = new Fruit("Cherry", R.mipmap.ic_launcher);
+            fruitList.add(cherry);
+            Fruit mango = new Fruit("Mango", R.mipmap.ic_launcher);
+            fruitList.add(mango);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL); // 设置横向滑动
+        recyclerView.setLayoutManager(layoutManager);
+        FruitAdapter adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
     }
 }
+
+class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getImageId() {
+        return this.imageId;
+    }
+}
+class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(View view) {
+            super(view);
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName  = (TextView) view.findViewById(R.id.fruit_name);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        // 纵向滑动
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item, parent, false);
+
+        // 横向滑动
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item_hr, parent, false);
+
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+    }
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+}
+
