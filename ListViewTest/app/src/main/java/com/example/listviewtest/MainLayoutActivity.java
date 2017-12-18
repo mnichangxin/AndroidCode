6f0c6bac8bbe9db520e6324536108fa0368cfb61
mnichangxin <mnichangxin@163.com>
2017-12-18 16:31:49 +0800

    commit

--- ListViewTest/app/src/main/java/com/example/listviewtest/MainLayoutActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainLayoutActivity.java
@@ -1,13 +1,98 @@
 package com.example.listviewtest;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainLayoutActivity extends AppCompatActivity {
+//    private String[] data = {
+//        "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango", "Apple", "Banana", "Orange"
+//    };
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private void  initFruits() {
+        for (int i = 0; i < 2; i++) {
+            Fruit apple = new Fruit("Apple", R.mipmap.ic_launcher);
+            fruitList.add(apple);
+            Fruit banana = new Fruit("Banana", R.mipmap.ic_launcher);
+            fruitList.add(banana);
+            Fruit orange = new Fruit("Orange", R.mipmap.ic_launcher);
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit("Watermelon", R.mipmap.ic_launcher);
+            fruitList.add(watermelon);
+            Fruit pear = new Fruit("Pear", R.mipmap.ic_launcher);
+            fruitList.add(pear);
+            Fruit grape = new Fruit("Grape", R.mipmap.ic_launcher);
+            fruitList.add(grape);
+            Fruit pineapple = new Fruit("Pineapple", R.mipmap.ic_launcher);
+            fruitList.add(pineapple);
+            Fruit strawberry = new Fruit("Strawberry", R.mipmap.ic_launcher);
+            fruitList.add(strawberry);
+            Fruit cherry = new Fruit("Cherry", R.mipmap.ic_launcher);
+            fruitList.add(cherry);
+            Fruit mango = new Fruit("Mango", R.mipmap.ic_launcher);
+            fruitList.add(mango);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(MainLayoutActivity.this, R.layout.fruit_item, fruitList);
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainLayoutActivity.this, android.R.layout.simple_expandable_list_item_1, data);
+
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+    }
+}
+
+class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getImageId() {
+        return this.imageId;
+    }
+}
+
+class FruitAdapter extends ArrayAdapter<Fruit> {
+    private int resourceId;
+
+    public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects) {
+        super(context, textViewResourceId, objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+        TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());
+        return view;
     }
 }
