6ef5ad5db84cc02ac0715864de76233a696566ff
mnichangxin <mnichangxin@163.com>
2017-12-20 22:55:56 +0800

    commit

---
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/News.java
@@ -0,0 +1,19 @@
+package com.example.fragmentbestpractice;
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return this.title;
+    }
+    public String getContent() {
+        return this.content;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
