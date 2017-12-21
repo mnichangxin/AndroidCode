7280159fc2a1cd388af0cd87bad3a10a0fb5988e
mnichangxin <mnichangxin@163.com>
2017-12-21 23:13:21 +0800

    commit

--- FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -2,9 +2,13 @@ package com.example.fragmentbestpractice;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class NewsTitleFragment extends Fragment {
     private boolean isTwoPane;
@@ -21,6 +25,27 @@ public class NewsTitleFragment extends Fragment {
             isTwoPane = true;
         } else {
             isTwoPane = false;
+        }
+    }
+
+    class NewAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
+        private List<News> mNewsList;
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            TextView newsTitleText;
+
+            public ViewHolder(View view) {
+                super(view);
+                newsTitleText = (TextView) view.findViewById(R.id.news_title);
+            }
+        }
+        public NewsAdapter(List<News> newsList) {
+            mNewsList = newsList;
+        }
+
+        @Override
+        public NewsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
         }
     }
 }
