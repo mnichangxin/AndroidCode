62e2537945b6ee32d90c911cb965f4e8c43bd7d0
mnichangxin <mnichangxin@163.com>
2017-12-25 22:13:43 +0800

    commit

--- DatabaseTest/app/src/main/java/com/example/databasetest/MainLayoutActivity.java
+++ DatabaseTest/app/src/main/java/com/example/databasetest/MainLayoutActivity.java
@@ -1,5 +1,7 @@
 package com.example.databasetest;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,10 +17,29 @@ public class MainLayoutActivity extends AppCompatActivity {
         dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
 
         Button createDatabase = (Button) findViewById(R.id.create_database);
+        Button addData = (Button) findViewById(R.id.add_data);
+
         createDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
             }
         });
     }
