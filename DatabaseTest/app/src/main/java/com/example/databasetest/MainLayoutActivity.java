aa79d729eca975b35432ef0c04bb431f3cca59f3
mnichangxin <mnichangxin@163.com>
2017-12-26 17:40:28 +0800

    commit

--- DatabaseTest/app/src/main/java/com/example/databasetest/MainLayoutActivity.java
+++ DatabaseTest/app/src/main/java/com/example/databasetest/MainLayoutActivity.java
@@ -1,9 +1,11 @@
 package com.example.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -18,13 +20,18 @@ public class MainLayoutActivity extends AppCompatActivity {
 
         Button createDatabase = (Button) findViewById(R.id.create_database);
         Button addData = (Button) findViewById(R.id.add_data);
+        Button updateData = (Button) findViewById(R.id.update_data);
+        Button deleteButton = (Button) findViewById(R.id.delete_data);
+        Button queryButton = (Button) findViewById(R.id.query_data);
 
+        // 创建数据
         createDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
             }
         });
+        // 插入数据
         addData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +47,46 @@ public class MainLayoutActivity extends AppCompatActivity {
                 values.put("pages", 510);
                 values.put("price", 19.95);
                 db.insert("Book", null, values);
+            }
+        });
+        // 更新数据
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                db.update("Book", values, "name = ?", new String[] { "The Da Vinci Code" });
+            }
+        });
+        // 删除数据
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book", "pages > ?", new String[] { "500" });
+            }
+        });
+        // 查询数据
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("price"));
+                        double prices = cursor.getDouble(cursor.getColumnIndex("price"));
+
+                        Log.d("MainActivity", "book name is " + name);
+                        Log.d("MainActivity", "book author is " + author);
+                        Log.d("MainActivity", "book pages is " + pages);
+                        Log.d("MainActivity", "book price is " + prices);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
