91588f6085c17c68970d0805d2ee4bbed4924517
mnichangxin <mnichangxin@163.com>
2017-12-26 22:23:39 +0800

    commit

--- RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainLayoutActivity.java
+++ RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainLayoutActivity.java
@@ -1,7 +1,11 @@
 package com.example.runtimepermissiontest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainLayoutActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainLayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        Button makecall = (Button) findViewById(R.id.make_call);
+        makecall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_CALL);
+                    intent.setData(Uri.parse("tel:10086"));
+                    startActivity(intent);
+                } catch (SecurityException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
