fd9eab937c5bb22db469c9e2c8410ffc04d9629c
mnichangxin <mnichangxin@163.com>
2017-12-27 23:24:58 +0800

    commit

--- ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.java
+++ ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
 
