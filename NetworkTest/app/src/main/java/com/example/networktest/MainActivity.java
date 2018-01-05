a09a588af51e18f0d5d43dc4fa21a25f668b065d
mnichangxin <mnichangxin@163.com>
2018-01-05 21:46:56 +0800

    commit

--- NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
+++ NetworkTest/app/src/main/java/com/example/networktest/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.networktest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.view.menu.ExpandedMenuView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
