007e6e7df9759709924b72367093682acf63929b
mnichangxin <mnichangxin@163.com>
2018-01-13 20:49:40 +0800

    commit

--- ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             downloadBinder.getProgress();
         }
         @Override
-        public void onServiceDisconnected(ComponentName name) {
+        public void onServiceDisconnected(ComponentName name) {\
+            
         }
     };
 
