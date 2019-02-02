52c466e8bcf175d86581019bd193f9f4a40a64f3
mnichangxin <mnichangxin@163.com>
2019-02-02 21:11:10 +0800

    add custom views

---
+++ CustomViews/Measure/app/src/main/java/com/customviews/measure/MainActivity.java
@@ -0,0 +1,24 @@
+package com.customviews.measure;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
+class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+    }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
---
+++ CustomViews/README.md
@@ -0,0 +1,1 @@
+# Android 自定义 View
